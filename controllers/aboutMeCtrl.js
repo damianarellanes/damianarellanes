5462e8467a4df2af2d48d5c723a4f7bc15bf6e06
damianarellanes <damian.arellanes@lancaster.ac.uk>
2022-06-21 11:51:06 +0100

    Removed

--- controllers/aboutMeCtrl.js
+++ controllers/aboutMeCtrl.js
@@ -1,6 +1,6 @@
 myWebsite.controller('aboutMeCtrl', function ($scope) {
 	
 	// See models/aboutMeModel
-  $scope.aboutMeSections = [resume, researchInterests, otherPublications, pubs, reviews, teaching];
+  $scope.aboutMeSections = [resume, researchInterests, otherPublications, pubs, reviews];
     
 });
