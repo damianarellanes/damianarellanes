763984572bf070e91eee1f3b5be08434b7ba44af
damianarellanes <damian.arellanes@gmail.com>
2025-12-20 04:31:40 +0000

    added may news

--- models/newsModel.js
+++ models/newsModel.js
@@ -1,20 +1,24 @@
 /**************************** NEWS ****************************/
 var n1 = {
         title: 'April 2024',
-    info : 'My <b>paper</b> on <a style="color:#000a56" target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-031-64626-3_2">Composition Machines for the emergence of Program Spaces</a> has been accepted for publication at the International Symposium on Theoretical Aspects of Software Engineering (TASE 2024).'
+    info : 'My <b>paper</b> on <a style="color:#000a56" target="_blank" href="https://link.springer.com/chapter/10.1007/978-3-031-64626-3_2">Composition Machines for the emergence of Program Spaces</a> has been accepted for publication in the Proceedings of the International Symposium on Theoretical Aspects of Software Engineering (TASE 2024).'
 };
 var n2 = {
+        title: 'May 2025',
+    info : 'My introductory <b>paper</b> on <a style="color:#000a56" target="_blank" href="https://www.readcube.com/articles/10.3389/fcomp.2025.1564048">Models of High-level Computation</a> has been accepted for publication in the journal Frontiers of Computer Science.'
+};
+var n3 = {
         title: 'October 2025',
     info : 'I am excited to deliver a <b>research seminar</b> at the <a style="color:#000a56" target="_blank" href="http://pplv.cs.ucl.ac.uk/welcome/">PPLV group</a> at <a style="color:#000a56" target="_blank" href="https://www.ucl.ac.uk">UCL</a>, where I will be discussing algebraic composition in Models of High-Level Computation.'
 };
-var n3 = {
+var n4 = {
         title: 'December 2025',
     info : 'I am delighted to announce that I will deliver a <b>research seminar</b> at the <a style="color:#000a56" target="_blank" href="https://www.cs.manchester.ac.uk">Department of Computer Science</a> at <a style="color:#000a56" target="_blank" href="https://www.manchester.ac.uk">The University of Manchester</a> on February 4, 2025. In this talk, I will introduce a category-theoretic framework for algebraically composing high-level computing devices.'
 };
 var news = {
 	title: 'Recent News',
 	body: [
-	    { type: 'news', listType: 'ul', content: [n3, n2, n1] }
+	    { type: 'news', listType: 'ul', content: [n4, n3, n2, n1] }
 	]
 };
 /**************************** END: NEWS ****************************/
