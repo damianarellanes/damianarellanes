f159edd25c529589bfa921d539cd4f3453c6da9e
damianarellanes <damian.arellanes@gmail.com>
2025-12-20 17:44:43 +0000

    change data news

--- models/newsModel.js
+++ models/newsModel.js
@@ -9,11 +9,11 @@ var n2 = {
 };
 var n3 = {
         title: 'September 2025',
-    info : 'I am excited to deliver a <b>research seminar</b> at the <a style="color:#000a56" target="_blank" href="http://pplv.cs.ucl.ac.uk/welcome/">PPLV group</a> at <a style="color:#000a56" target="_blank" href="https://www.ucl.ac.uk">UCL</a> on October 30, 2025. In this talk, I will be discussing algebraic composition in Models of High-Level Computation.'
+    info : 'I am excited to deliver a <b>research seminar</b> to the <a style="color:#000a56" target="_blank" href="http://pplv.cs.ucl.ac.uk/welcome/">PPLV group</a> at <a style="color:#000a56" target="_blank" href="https://www.ucl.ac.uk">UCL</a> on October 30, 2025. In this talk, I will be discussing algebraic composition in Models of High-Level Computation.'
 };
 var n4 = {
         title: 'December 2025',
-    info : 'I am delighted to announce that I will deliver a <b>research seminar</b> at the <a style="color:#000a56" target="_blank" href="https://www.cs.manchester.ac.uk">Department of Computer Science</a> at <a style="color:#000a56" target="_blank" href="https://www.manchester.ac.uk">The University of Manchester</a> on February 4, 2025. In this talk, I will introduce a category-theoretic framework for algebraically composing high-level computing devices.'
+    info : 'I am delighted to announce that I will deliver a <b>research seminar</b> at the <a style="color:#000a56" target="_blank" href="https://www.cs.manchester.ac.uk">Department of Computer Science</a> at <a style="color:#000a56" target="_blank" href="https://www.manchester.ac.uk">The University of Manchester</a> on February 4, 2026. In this talk, I will introduce a category-theoretic framework for algebraically composing high-level computing devices.'
 };
 var news = {
 	title: 'Recent News',
